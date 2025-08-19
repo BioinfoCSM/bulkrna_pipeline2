e91285ed0d40f3b477f4520492dff28f33bb2dce
BioinfoCSM <simoncheng158@gmail.com>
2025-08-19 14:04:28 +0800

    modify script version

--- script/featureCounts.R
+++ script/featureCounts.R
@@ -2,7 +2,7 @@
 
 #======================#
 #author : BioinfoCSM   #
-#version : 1.0         #
+#version : 0.1         #
 #======================#
 
 #===load packages===
--- script/pca.R
+++ script/pca.R
@@ -2,7 +2,7 @@
 
 #==========================#
 #author : BioninfoCSM      #
-#version : 1.0             #
+#version : 0.1             #
 #==========================#
 
 #===load packages====
